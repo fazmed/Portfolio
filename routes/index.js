8a24a034782ce43f2d0882e7a449ada60bca999c
farhan0710@gmail.com <farhan0710@gmail.com>
2022-06-09 19:27:19 -0400

    final touches - nav menu size adjust - small error fixes

--- routes/index.js
+++ routes/index.js
@@ -3,31 +3,31 @@ var router = express.Router();
 
 /* GET Home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home'});
+  res.render('index', { title: 'home'});
 });
 
 router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'Home'});
+  res.render('index', { title: 'home'});
 });
 
 /* GET About Us page. */
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'About'});
+  res.render('about', { title: 'about'});
 });
 
 /* GET Products page. */
 router.get('/projects', function(req, res, next) {
-  res.render('projects', { title: 'Projects'});
+  res.render('projects', { title: 'projects'});
 });
 
 /* GET Services page. */
 router.get('/services', function(req, res, next) {
-  res.render('services', { title: 'Services'});
+  res.render('services', { title: 'services'});
 });
 
 /* GET Contact Us page. */
 router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'Contact'});
+  res.render('contact', { title: 'contact'});
 });
 
 module.exports = router;
